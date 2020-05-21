8f7b14000a149c4d98e1502a2386f7dd0be8fe28
gsalazar <gsalazargisbert@gmail.com>
2020-05-21 12:56:13 -0300

    Migration from Renderer to Renderer2

--- projects/ngx-neo-completer-mat/src/lib/directives/ctr-row.ts
+++ projects/ngx-neo-completer-mat/src/lib/directives/ctr-row.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Host, HostListener, Input, Renderer, OnDestroy } from '@angular/core';
+import { Directive, ElementRef, Host, HostListener, Input, Renderer2, OnDestroy } from '@angular/core';
 
 import { CompleterItem } from '../components/completer-item';
 import { CtrDropdown, CtrRowElement, CtrRowItem } from './ctr-dropdown';
@@ -12,7 +12,7 @@ export class CtrRow implements CtrRowElement, OnDestroy {
     private _rowIndex: number;
     private _item: CompleterItem;
 
-    constructor(private el: ElementRef, private renderer: Renderer, @Host() private dropdown: CtrDropdown) { }
+    constructor(private el: ElementRef, private renderer: Renderer2, @Host() private dropdown: CtrDropdown) { }
 
     public ngOnDestroy() {
         if (this._rowIndex) {
@@ -45,7 +45,8 @@ export class CtrRow implements CtrRowElement, OnDestroy {
 
     public setHighlighted(selected: boolean) {
         this.selected = selected;
-        this.renderer.setElementClass(this.el.nativeElement, 'completer-selected-row', this.selected);
+        const highlightClass: string = 'completer-selected-row';
+        this.selected ? this.renderer.addClass(this.el.nativeElement, highlightClass) : this.renderer.removeClass(this.el.nativeElement, highlightClass);
     }
 
     public getNativeElement() {
